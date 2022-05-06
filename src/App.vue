63cd4cfa43b56735a93b193d83df1c4b9c9b8dba
djkcyl <cyl@cyllive.cn>
2022-05-06 16:39:50 +0800

    latest

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="px-5 m-auto mt-5 flex justify-center">
+    <video id="video-webrtc" controls></video>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref } from "vue";
+
+const room = ref("djkcyl");
+onMounted(() => {
+  const video = document.getElementById("video-webrtc");
+  const player = new JSWebrtc.Player("webrtc://a60.one/live/" + room.value, {
+    video: video,
+    autoplay: true,
+    onPlay: () => {
+      document.title = "▶️ 当前直播 " + room.value;
+    },
+  });
+});
+</script>
