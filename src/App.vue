e4385f2686a32d9d67f6fe48df75ce8533ebf98f
baka-gourd <36119339+baka-gourd@users.noreply.github.com>
2022-05-06 18:09:14 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,44 @@
 <template>
-  <div class="px-5 m-auto mt-5 flex justify-center">
-    <video id="video-webrtc" controls></video>
+  <div class="px-5 mx-auto mt-5 justify-center container">
+    <input type="text" v-model="room" />
+    <input type="checkbox" name="rtc" id="rtc" v-model="irtc">
+    <video v-show="irtc" id="video-webrtc" controls></video>
+    <div  v-show="!irtc" id="video-flv"></div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref } from "vue";
+import DPlayer from "dplayer";
+import { onMounted, ref, watch } from "vue";
 
-const room = ref("djkcyl");
-onMounted(() => {
+let room = ref("djkcyl");
+let irtc = ref(false);
+
+watch(room, () => apply());
+watch(irtc, () => apply());
+
+onMounted(() => apply());
+
+function apply() {
+  const rtc = "webrtc://a60.one/live/" + room.value;
+  const flv = `http://a60.one:8090/live/${room.value}.flv`;
   const video = document.getElementById("video-webrtc");
-  const player = new JSWebrtc.Player("webrtc://a60.one/live/" + room.value, {
-    video: video,
-    autoplay: true,
-    onPlay: () => {
-      document.title = "▶️ 当前直播 " + room.value;
-    },
-  });
-});
+  const video2 = document.getElementById("video-flv");
+  if (irtc.value) {
+    const player = new JSWebrtc.Player(rtc, {
+      video: video,
+      autoplay: true
+    });
+  } else {
+    const dp = new DPlayer({
+      container: video2,
+      autoplay: true,
+      video: {
+        name: "flv",
+        url: flv,
+        type: "flv",
+      },
+    });
+  }
+}
 </script>
